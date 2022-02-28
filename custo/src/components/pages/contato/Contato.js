58c0dc1d7948c3bcbf1ac1aebcaf305c1ec3661c
janisson.carlos <janisson.carlos@objetivasolucao.com.br>
2022-02-28 18:05:42 -0300

    commit inicial

---
+++ custo/src/components/pages/contato/Contato.js
@@ -0,0 +1,8 @@
+function Contato (){
+    return (
+        <div>
+            <h1>contato</h1>
+        </div>
+    )
+}
+export default Contato
---
+++ custo/src/components/pages/newProject/NewProject.js
@@ -0,0 +1,8 @@
+function NewProject(){
+    return (
+        <>
+        <h1>NewProject</h1>
+        </>
+    )
+}
+export default NewProject
